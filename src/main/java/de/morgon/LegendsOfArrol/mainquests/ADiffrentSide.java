5df7155651fe030ac1d586107ccde3013943a74d
Morgon <moritz.olthoff@gmail.com>
2022-12-11 09:54:18 +0100

    renaming quest package

---
+++ src/main/java/de/morgon/LegendsOfArrol/mainquests/ADiffrentSide.java
@@ -0,0 +1,26 @@
+package de.morgon.LegendsOfArrol.mainquests;
+
+/*
+This quest immediately starts when the player joins the game
+*/
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class ADiffrentSide implements Listener {
+
+    @EventHandler
+    public void onSpawn(BlockBreakEvent e){
+        Player p = e.getPlayer();
+        if(p.getScoreboard().getScores("storyquest").equals(1)){
+        if(e.getBlock().equals(Material.WHEAT)){
+            p.sendMessage("§4Sehr gut!");
+            p.sendMessage("§4Sammle insgesammt 10 Weizen und bringe sie zur Hafenstadt. Du findest sie, wenn du dem Weg hinter dem Haus folgst");
+            }
+        }
+    }
+
+}
