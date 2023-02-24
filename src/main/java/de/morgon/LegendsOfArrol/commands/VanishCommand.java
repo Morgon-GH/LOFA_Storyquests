aa11aefba88b2017aad1805ce17cf3dff5f36902
TheFlameB <120961852+TheFlameB@users.noreply.github.com>
2023-02-24 17:21:06 +0100

    Added Clearchatbefehl & fixed Bugs

--- src/main/java/de/morgon/LegendsOfArrol/commands/VanishCommand.java
+++ src/main/java/de/morgon/LegendsOfArrol/commands/VanishCommand.java
@@ -5,15 +5,45 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
 
 public class VanishCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
 
-        Player p = Bukkit.getPlayer(commandSender.getName());
+        ArrayList V = new ArrayList<>();
 
         if (strings.length == 1) {
+            for (Player onlinePlayers: Bukkit.getOnlinePlayers()) {
+                Player pn = Bukkit.getPlayer(strings[0]);
+                if (commandSender.hasPermission("LegendsOfArrol.vanish")) {
+                    if (V.contains(pn)) {
+                        pn.showPlayer((Plugin) this, onlinePlayers);
+                        commandSender.sendMessage("Player " + strings[0] + " is now shown");
+                    } else {
+                        pn.hidePlayer((Plugin) this, onlinePlayers);
+                        commandSender.sendMessage("Player " + strings[0] + " is now hidden");
+                    }
+                }
+            }
 
+        }else {
+            for (Player onlinePlayers: Bukkit.getOnlinePlayers()) {
+                Player p = Bukkit.getPlayer(commandSender.getName());
+                if (commandSender.hasPermission("LegendsOfArrol.vanish")) {
+                    if (V.contains(commandSender)) {
+                        p.showPlayer((Plugin) this, onlinePlayers);
+                        commandSender.sendMessage("Player is now shown");
+                    }else {
+                        p.hidePlayer((Plugin) this, onlinePlayers);
+                        commandSender.sendMessage("Player is now hidden");
+                    }
+                }else{
+
+                }
+            }
         }
 
 
--- src/main/java/de/morgon/LegendsOfArrol/main.java
+++ src/main/java/de/morgon/LegendsOfArrol/main.java
@@ -1,23 +1,15 @@
 package de.morgon.LegendsOfArrol;
 
-import de.morgon.LegendsOfArrol.commands.Party;
-import de.morgon.LegendsOfArrol.commands.StartingFirstQuestCommand;
-import de.morgon.LegendsOfArrol.commands.Teamchat;
-import de.morgon.LegendsOfArrol.commands.TrackPlayerProgress;
+import de.morgon.LegendsOfArrol.commands.*;
 import de.morgon.LegendsOfArrol.configs.PlayerCoordinatesConfig;
 import de.morgon.LegendsOfArrol.configs.PlayerProgressConfig;
-import de.morgon.LegendsOfArrol.configs.TeammitgliederConfig;
 import de.morgon.LegendsOfArrol.mechanics.SavePlayerCoordinates;
 import de.morgon.LegendsOfArrol.mechanics.ControlBlockBreak;
 import de.morgon.LegendsOfArrol.mechanics.FarmingWheat;
 import de.morgon.LegendsOfArrol.mainquests.ADiffrentSide;
-import jdk.tools.jmod.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.Team;
-
-import java.util.MissingFormatArgumentException;
 
 public final class main extends JavaPlugin {
 
@@ -35,19 +27,16 @@ public final class main extends JavaPlugin {
         PlayerCoordinatesConfig.save();
         PlayerCoordinatesConfig.get().options().copyDefaults();
 
-        TeammitgliederConfig.setup();
-        TeammitgliederConfig.save();
-        TeammitgliederConfig.get().options().copyDefaults();
-
-
         System.out.println("Starting up plugin");
 
         getCommand("start1quest").setExecutor(new StartingFirstQuestCommand());
         getCommand("QuestProg").setExecutor(new TrackPlayerProgress());
         getCommand("Party").setExecutor(new Party());
         getCommand("P").setExecutor(new Party());
-        getCommand("Teamchat").setExecutor(new Teamchat());
-        getCommand("Tc").setExecutor(new Teamchat());
+        getCommand("V").setExecutor(new VanishCommand());
+        getCommand("Vanish").setExecutor(new VanishCommand());
+        getCommand("cch").setExecutor(new Clearchat());
+        getCommand("Clearchat").setExecutor(new Clearchat());
 
         PluginManager pluginManager= Bukkit.getPluginManager();
 
