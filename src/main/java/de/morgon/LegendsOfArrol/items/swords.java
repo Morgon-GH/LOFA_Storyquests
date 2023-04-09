2405807ca7d1c0081b98f6b39a994d26ca3c3e18
CrAzyA22 <klemm.anton@outlook.de>
2023-04-09 18:16:34 +0200

    Mehr tools / waffen (hab den command gefixt)

--- src/main/java/de/morgon/LegendsOfArrol/items/swords.java
+++ src/main/java/de/morgon/LegendsOfArrol/items/swords.java
@@ -26,7 +26,7 @@ public class swords implements CommandExecutor {
                     ItemStack i = new ItemStack(Material.IRON_AXE);
 
                     ItemMeta m = i.getItemMeta();
-                    m.setDisplayName("§Dwarf Battleblade");
+                    m.setDisplayName("§5Dwarf Battleblade");
                     m.setUnbreakable(true);
                     m.addEnchant(Enchantment.FROST_WALKER, 1, false);
                     m.addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_UNBREAKABLE);
@@ -35,6 +35,65 @@ public class swords implements CommandExecutor {
                     lore.add("§7A rare battleaxe");
                     lore.add("§7forged out of the");
                     lore.add("§7purest dwarf metals.");
+                    lore.add(" ");
+                    lore.add("§7You can throw it!");
+                    m.setLore(lore);
+                    i.setItemMeta(m);
+
+                    p.getInventory().addItem(i);
+
+                }else
+                if(args[0].equals("reaper_scythe")) {
+                    ItemStack i = new ItemStack(Material.DIAMOND_HOE);
+
+                    ItemMeta m = i.getItemMeta();
+                    m.setDisplayName("§5Reaper Scythe");
+                    m.setUnbreakable(true);
+                    m.addEnchant(Enchantment.FROST_WALKER, 1, false);
+                    m.addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_UNBREAKABLE);
+                    ArrayList lore = new ArrayList();
+                    lore.add(" ");
+                    lore.add("§7A wonderful tool");
+                    lore.add("§7used to summon lost souls");
+                    lore.add("§7from pure death...");
+                    m.setLore(lore);
+                    i.setItemMeta(m);
+
+                    p.getInventory().addItem(i);
+
+                }else
+                if(args[0].equals("fanatic_catana")) {
+                    ItemStack i = new ItemStack(Material.DIAMOND_SWORD);
+
+                    ItemMeta m = i.getItemMeta();
+                    m.setDisplayName("§eFanatic Catana");
+                    m.setUnbreakable(true);
+                    m.addEnchant(Enchantment.FROST_WALKER, 1, false);
+                    m.addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_UNBREAKABLE);
+                    ArrayList lore = new ArrayList();
+                    lore.add(" ");
+                    lore.add("§7It's swings are pure");
+                    lore.add("§7void. And the void ");
+                    lore.add("§7always strikes two times...");
+                    m.setLore(lore);
+                    i.setItemMeta(m);
+
+                    p.getInventory().addItem(i);
+
+                }else
+                if(args[0].equals("shadow_claymore")) {
+                    ItemStack i = new ItemStack(Material.NETHERITE_SWORD);
+
+                    ItemMeta m = i.getItemMeta();
+                    m.setDisplayName("§bShadow Claymore");
+                    m.setUnbreakable(true);
+                    m.addEnchant(Enchantment.FROST_WALKER, 1, false);
+                    m.addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_UNBREAKABLE);
+                    ArrayList lore = new ArrayList();
+                    lore.add(" ");
+                    lore.add("§7This hunk of stone is");
+                    lore.add("§7too big to be even");
+                    lore.add("§7called a sword...");
                     m.setLore(lore);
                     i.setItemMeta(m);
 
