ef41811271c82ef3e878bf8ad89f766a21711c8d
Morgon-GH <moritz.olthoff@gmail.com>
2023-08-25 14:54:13 +0200

    npc

--- src/main/java/de/morgon/LegendsOfArrol/npc/NPCSpawnCommand.java
+++ src/main/java/de/morgon/LegendsOfArrol/npc/NPCSpawnCommand.java
@@ -1,19 +1,24 @@
 package de.morgon.LegendsOfArrol.npc;
 
 import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import net.minecraft.network.protocol.game.ClientboundAddPlayerPacket;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_20_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import de.morgon.LegendsOfArrol.Message;
 
+import java.util.Collection;
+import java.util.Optional;
 import java.util.UUID;
 
 public class NPCSpawnCommand implements CommandExecutor {
@@ -25,12 +30,21 @@ public class NPCSpawnCommand implements CommandExecutor {
 
         if (commandSender instanceof Player){
 
-            CraftPlayer craftPlayer = (CraftPlayer) commandSender;
+            Player p = (Player) commandSender;
+
+            CraftPlayer craftPlayer = (CraftPlayer) p;
             ServerPlayer sp = (ServerPlayer) craftPlayer.getHandle();
 
             MinecraftServer server = sp.getServer();
-            ServerLevel level = (ServerLevel) craftPlayer.getWorld();
-            GameProfile gameProfile = new GameProfile(UUID.randomUUID(), "strings[0]");
+            ServerLevel level = ((CraftWorld)p.getWorld()).getHandle();
+            GameProfile gameProfile = new GameProfile(UUID.randomUUID(), strings[0]);
+
+            Player skinPlayer = Bukkit.getPlayer(strings[1]);
+            ServerPlayer skinServerPlayer = ((CraftPlayer)skinPlayer).getHandle();
+            Collection<Property> skinCollection = skinServerPlayer.getGameProfile().getProperties().get("textures");
+            Property skin = skinCollection.stream().findFirst().get();
+
+            gameProfile.getProperties().put("textures", skin);
 
             ServerPlayer npc = new ServerPlayer(server, level, gameProfile);
             npc.setPos(((CraftPlayer) commandSender).getLocation().getBlockX(), ((CraftPlayer) commandSender).getLocation().getBlockY(), ((CraftPlayer) commandSender).getLocation().getBlockZ());
